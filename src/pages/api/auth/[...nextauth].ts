cff7b8f308cda60650ab763a4401fec09d884ad9
Cleisson <cleissonconstc@gmail.com>
2022-06-16 15:56:15 -0300

    FEAT: adding a function to create a user in DB and don't duplicate

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,6 @@
+import {query as q} from 'faunadb'
+import { fauna } from "../../../services/fauna"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
@@ -13,4 +16,36 @@ export default NextAuth({
       }
     }),
   ],
+  secret: process.env.SECRET,
+  callbacks: {
+    async signIn({ user }) {   
+      try {
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection("users"),
+            {data: {user}}
+          ),
+          q.Get(
+            q.Match(
+              q.Index("user_by_email"),
+              q.Casefold(user.email)
+            )
+          )
+        )
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }   
+    },
+  },
 })
+
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,4 +2,5 @@ import { Client } from 'faunadb'
 
 export const fauna = new Client({
   secret: process.env.FAUNA_DB_KEY,
+  domain: "db.fauna.com",
 })
